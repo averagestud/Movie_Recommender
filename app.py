e1e64e951696a0198b82ed2bc4d6a4a428196707
Deepaprakash K <128608033+averagestud@users.noreply.github.com>
2024-07-05 15:01:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ import streamlit as st
 import pickle
 import pandas as pd
 import requests
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+import time
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 def fetch_poster(movie_id):
     response=requests.get('https://api.themoviedb.org/3/movie/{}?api_key=f19da43b6e95d951f63c85c751fee805&&language=en-US'.format(movie_id))
@@ -26,6 +34,7 @@ def recommend(movie):
 
 movies_dict=pickle.load(open('movies_dict.pkl','rb'))
 movies=pd.DataFrame(movies_dict)
+local_css("styles.css")
 
 similarity=pickle.load(open('similarity.pkl','rb'))
 
@@ -36,33 +45,22 @@ selected_movie_name = st.selectbox(
     (movies['title'].values))
 
 if st.button('Recommend'):
-    names,posters=recommend(selected_movie_name)
+    names, posters = recommend(selected_movie_name)
     
-    col1,col2,col3,col4,col5,col6,col7,col8,col9=st.columns(9)
-    with col1:
-        st.text(names[0])
-        st.image(posters[0])
-    with col2:
-        st.text(names[1])
-        st.image(posters[1])
-    with col3:
-        st.text(names[2])
-        st.image(posters[2])
-    with col4:
-        st.text(names[3])
-        st.image(posters[3])
-    with col5:
-        st.text(names[4])
-        st.image(posters[4])
-    with col6:
-        st.text(names[5])
-        st.image(posters[5])
-    with col7:
-        st.text(names[6])
-        st.image(posters[6])
-    with col8:
-        st.text(names[7])
-        st.image(posters[7])
-    with col9:
-        st.text(names[8])
-        st.image(posters[8])
+    # Create a container for the movies
+    st.markdown('<div class="movie-container">', unsafe_allow_html=True)
+    
+    for i in range(len(names)):
+        
+        movie_search_url = f"https://www.google.com/search?q={names[i]}"
+        movie_html = f'''
+            <div class="movie">
+                <a href="{movie_search_url}" target="_blank">
+                    <p>{names[i]}</p>
+                    <img src="{posters[i]}" class="movie-poster" />
+                </a>
+            </div>
+        '''
+        st.markdown(movie_html, unsafe_allow_html=True)
+    
+    st.markdown('</div>', unsafe_allow_html=True)
